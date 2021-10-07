b6f32895f03d3c5ea8300b89cf15a846efccf1f3
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2021-10-07 19:35:27 -0300

    style: 💄 change the weather icons to svg

--- src/components/functions/Climate/index.jsx
+++ src/components/functions/Climate/index.jsx
@@ -10,13 +10,13 @@ export default function Climate() {
     const {currentWeather} = useContext(WeatherContext)
     const condition = currentWeather?.condition;
 
- 
+
     return(
 
         <div className={styles.climate}>
             <h3 className={styles.city}>{currentWeather?.city}</h3>
              <h4 className={styles.country}>{currentWeather?.country}</h4>
-             {/* <p className={styles.text}> <Image src={currentIcon(condition).src} width="20rem" height="20rem" /> {" "}{currentWeather?.condition}</p> */}
+             <p className={styles.text}> <Image src={currentIcon(condition)} width="20rem" height="20rem" /> {" "}{currentWeather?.condition}</p>
              <h2 className={styles.celsius}> {currentWeather?.temperature}°c</h2>
              <p className={styles.text}>{currentWeather?.date}</p>
         </div>
