a1b05bb51af5287a816a2e0f33c80d1770d336fe
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2021-10-08 14:39:07 -0300

    fix: 🐛 fetch error

--- src/services/LastFm/index.js
+++ src/services/LastFm/index.js
@@ -2,6 +2,9 @@ const Key = process.env.NEXT_PUBLIC_PUBLISHABLE_KEY;
 
 export default async function getPlaylist(genre) {
   const url = `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${genre}&limit=15&api_key=${Key}&format=json`;
-  return await fetch(url).then((response) => response.json());
+  return await fetch(url, {
+    method: "GET",
+    headers: {
+        "Content-Type": "text/plain"
+    }}).then((response) => response.json());
 }
-
